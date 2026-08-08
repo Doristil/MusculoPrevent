a001c4caaeb58aea84d93c3b2f2aef60778e4181
Doristil <oristil.doriann@gmail.com>
2026-08-08 11:55:17 +0200

    Refine feminine body map silhouette

--- src/components/BodyMap.jsx
+++ src/components/BodyMap.jsx
@@ -26,17 +26,17 @@ const splitFaceBackZones = new Set(["epaules", "bras", "avant-bras", "main-poign
 function BaseBody({ x, back = false, sex = "male" }) {
   const isFemale = sex === "female";
   const torsoPath = isFemale
-    ? "M133 80c-18 5-32 14-44 28 7 9 15 16 26 20 2 16 5 31 3 45l-7 52c-3 20-9 39-5 55 4 15 15 25 30 30l2 31h24l2-31c15-5 26-15 30-30 4-16-2-35-5-55l-7-52c-2-14 1-29 3-45 11-4 19-11 26-20-12-14-26-23-44-28-7 10-28 10-35 0z"
+    ? "M134 80c-20 4-37 14-51 29 8 10 18 17 30 20-2 17 1 34 2 49l-8 49c-4 22-10 43-4 58 5 14 17 23 32 28l2 28h26l2-28c15-5 27-14 32-28 6-15 0-36-4-58l-8-49c1-15 4-32 2-49 12-3 22-10 30-20-14-15-31-25-51-29-8 11-29 11-38 0z"
     : "M131 81c-20 5-37 15-50 31 8 10 18 17 29 21 1 20 4 39 1 60l-6 68c1 17 10 29 22 36l3 29h40l3-29c12-7 21-19 22-36l-6-68c-3-21 0-40 1-60 11-4 21-11 29-21-13-16-30-26-50-31z";
   const legPath = isFemale
-    ? "M121 280c-13 28-14 64-4 94l10 28-3 91 22 10 5-120-1-99c-10-5-20-6-29-4zM179 280c13 28 14 64 4 94l-10 28 3 91-22 10-5-120 1-99c10-5 20-6 29-4z"
+    ? "M116 279c-15 31-13 68 2 98l11 25-4 90 23 11 4-121-2-99c-12-6-23-7-34-4zM184 279c15 31 13 68-2 98l-11 25 4 90-23 11-4-121 2-99c12-6 23-7 34-4z"
     : "M126 283c-13 28-14 67-4 105l8 24-3 88 22 9 2-127-1-96zM174 283c13 28 14 67 4 105l-8 24 3 88-22 9-2-127 1-96z";
   const armPath = isFemale
-    ? "M105 125c-12 17-17 39-15 63l3 66c1 17 6 31 15 42l10-5-4-73 9-50zM195 125c12 17 17 39 15 63l-3 66c-1 17-6 31-15 42l-10-5 4-73-9-50z"
+    ? "M103 126c-11 18-15 39-13 62l3 65c1 17 5 30 14 41l9-5-3-71 8-49zM197 126c11 18 15 39 13 62l-3 65c-1 17-5 30-14 41l-9-5 3-71-8-49z"
     : "M103 126c-14 16-20 41-18 66l2 67c1 17 7 31 17 42l11-5-4-77 10-54zM197 126c14 16 20 41 18 66l-2 67c-1 17-7 31-17 42l-11-5 4-77-10-54z";
   return <g className={`body-base ${isFemale ? "body-female" : "body-male"}`} transform={`translate(${x} 0)`}>
     <ellipse className="body-head" cx="150" cy="46" rx="22" ry="29" />
-    {isFemale && <path className="body-hair" d={back ? "M126 35c1-22 47-22 48 0l2 67-11 7-5-42h-20l-5 42-11-7z" : "M126 35c2-22 46-22 48 0l2 39-9 7-4-26h-26l-4 26-9-7z"} />}
+    {isFemale && <path className="body-hair" d={back ? "M126 35c1-22 47-22 48 0l2 65c11 7 13 17 8 25-6 5-15 1-17-7l-7-50h-20l-7 50c-2 8-11 12-17 7-5-8-3-18 8-25z" : "M126 35c2-22 46-22 48 0l2 39-9 7-4-26h-26l-4 26-9-7z"} />}
     {!back ? <g className="merchant-cap" transform="translate(0 -13)"><path className="cap-crown" d="M122 37c3-17 53-17 56 0l-2 13c-9 5-43 5-52 0z" /><path className="cap-band" d="M124 49c10 4 42 4 52 0v9c-12 4-40 4-52 0z" /><path className="cap-gold cap-gold-one" d="M126 51c12 3 36 3 48 0" /><path className="cap-gold cap-gold-two" d="M126 55c12 3 36 3 48 0" /><path className="cap-visor" d="M126 58c14 5 34 5 48 0 1 6-8 10-24 10s-25-4-24-10z" /><circle className="cap-badge" cx="150" cy="43" r="6" /><path className="cap-anchor" d="M150 39v7m-3-3h6m-7 2c1 4 8 4 10 0m-10 0c-2 0-3-2-3-3m10 3c2 0 3-2 3-3" /></g> : <g className="merchant-cap" transform="translate(0 -13)"><path className="cap-crown" d="M122 37c3-17 53-17 56 0l-2 19c-13 4-39 4-52 0z" /><path className="cap-band" d="M124 49c10 4 42 4 52 0v9c-12 4-40 4-52 0z" /><path className="cap-gold cap-gold-one" d="M126 51c12 3 36 3 48 0" /><path className="cap-gold cap-gold-two" d="M126 55c12 3 36 3 48 0" /></g>}
     <path className="body-neck" d="M137 69c1 7-1 12-6 17h38c-5-5-7-10-6-17z" />
     <path className="body-torso" d={torsoPath} />
@@ -44,7 +44,7 @@ function BaseBody({ x, back = false, sex = "male" }) {
     <path className="body-hand" d="M87 291c-6 9-5 24 1 34l10 9 8-4-2-27zM213 291c6 9 5 24-1 34l-10 9-8-4 2-27z" />
     <path className="body-leg" d={legPath} />
     <path className="body-foot" d="M127 499l22 10 10 8h-32zM173 499l-22 10-10 8h32z" />
-    {!back && <g className="anatomy-lines body-face"><path d="M139 42h8M153 42h8M146 57q4 4 8 0" /><path d="M150 89v178M111 142c14 12 25 16 39 16s25-4 39-16M112 143c12 0 26 5 38 15M188 143c-12 0-26 5-38 15M119 169c11 7 21 10 31 10s20-3 31-10M121 250c10 8 19 11 29 11s19-3 29-11" /></g>}
+    {!back && <g className="anatomy-lines body-face"><path d="M139 42h8M153 42h8M146 57q4 4 8 0" /><path d="M150 89v178M111 142c14 12 25 16 39 16s25-4 39-16M112 143c12 0 26 5 38 15M188 143c-12 0-26 5-38 15M119 169c11 7 21 10 31 10s20-3 31-10M121 250c10 8 19 11 29 11s19-3 29-11" />{isFemale && <path className="body-feminine-contours" d="M114 132c10 4 21 10 36 24 15-14 26-20 36-24M121 213c8 8 17 12 29 12s21-4 29-12M109 264c11 13 25 20 41 20s30-7 41-20" />}</g>}
     {back && <g className="anatomy-lines"><path className="spine" d="M150 88c-3 72-3 140 0 201" /><path d="M109 139c16 14 28 20 41 21 13-1 25-7 41-21M112 151c12 5 25 11 38 25M188 151c-12 5-25 11-38 25M112 258c13 13 25 19 38 20 13-1 25-7 38-20" /></g>}
   </g>;
 }
